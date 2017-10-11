f27896bce99f724c092526fe070925fed97e07ae
Malcom <gaeltshilombo@gmail.com>
2017-10-11 10:50:18 +0200

    first commit

---
+++ app/src/main/java/com/example/gael/poc_map_tracking_and_gallery/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.gael.poc_map_tracking_and_gallery;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
