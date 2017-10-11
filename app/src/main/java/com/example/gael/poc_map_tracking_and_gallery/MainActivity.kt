bd4414b2a3ecd0244e8a809bd4d782b2921daeb3
Malcom <gaeltshilombo@gmail.com>
2017-10-11 13:48:01 +0200

    use selector on bottom nav

--- app/src/main/java/com/example/gael/poc_map_tracking_and_gallery/MainActivity.kt
+++ app/src/main/java/com/example/gael/poc_map_tracking_and_gallery/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.gael.poc_map_tracking_and_gallery
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener{
+
+    var bottomNavigation : BottomNavigationView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigation = bottom_nav
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        return true
     }
 }
