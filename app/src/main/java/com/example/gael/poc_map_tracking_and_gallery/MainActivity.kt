046f50c1a0f37c4998431e17021b5c48ab684f1b
Malcom <gaeltshilombo@gmail.com>
2017-10-11 10:58:26 +0200

    convert first file to file Kotlin

---
+++ app/src/main/java/com/example/gael/poc_map_tracking_and_gallery/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.gael.poc_map_tracking_and_gallery
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
