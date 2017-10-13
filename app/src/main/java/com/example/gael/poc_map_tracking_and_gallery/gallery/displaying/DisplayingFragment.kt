90a88d8690032ba5ccdab72555dff6e9d815f560
Malcom <gaeltshilombo@gmail.com>
2017-10-13 13:09:55 +0200

    fragment used for preview

--- app/src/main/java/com/example/gael/poc_map_tracking_and_gallery/gallery/displaying/DisplayingFragment.kt
+++ app/src/main/java/com/example/gael/poc_map_tracking_and_gallery/gallery/displaying/DisplayingFragment.kt
@@ -1,10 +1,15 @@
 package com.example.gael.poc_map_tracking_and_gallery.gallery.displaying
 
+import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.gael.poc_map_tracking_and_gallery.R
+import com.example.gael.poc_map_tracking_and_gallery.models.Image
+import kotlinx.android.synthetic.main.fragment_displaying.*
 
 /**
  * Created on 12.10.17.
@@ -13,10 +18,17 @@ import android.view.ViewGroup
 class DisplayingFragment : Fragment(), DisplayingContract.View {
 
     private var presenter : DisplayingContract.Presenter? = null
+    private var image : Image? = null
 
     companion object {
-        fun newInstance() : DisplayingFragment {
-            return DisplayingFragment()
+        val KEY_IMAGE : String = "key_image"
+
+        fun newInstance(img : Image) : DisplayingFragment {
+            var frg : DisplayingFragment =  DisplayingFragment()
+            var arguments : Bundle = Bundle()
+            arguments.putParcelable(KEY_IMAGE,img)
+            frg.arguments = arguments
+            return frg
         }
     }
 
@@ -26,14 +38,29 @@ class DisplayingFragment : Fragment(), DisplayingContract.View {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        getArguments(arguments)
+    }
+
+    /**
+     * Get the image
+     */
+    private fun getArguments(arg : Bundle) {
+        if(arg.containsKey(KEY_IMAGE)){
+            image = arg.getParcelable(KEY_IMAGE)
+            Log.i("Test","image is ".plus(image.toString()))
+        }
     }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        return inflater!!.inflate(R.layout.fragment_displaying,container,false)
     }
 
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        title_image.text = image!!.type
+        preview.setImageURI(Uri.parse(image!!.uriImage))
     }
 
 }
