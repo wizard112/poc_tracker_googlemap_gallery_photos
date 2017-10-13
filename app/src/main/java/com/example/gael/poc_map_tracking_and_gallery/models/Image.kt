4bcb16745731597bf5741a6b2c73d7ee4aea8ad2
Malcom <gaeltshilombo@gmail.com>
2017-10-13 11:40:15 +0200

    add parcelable interface

--- app/src/main/java/com/example/gael/poc_map_tracking_and_gallery/models/Image.kt
+++ app/src/main/java/com/example/gael/poc_map_tracking_and_gallery/models/Image.kt
@@ -1,16 +1,27 @@
 package com.example.gael.poc_map_tracking_and_gallery.models
 
+import android.os.Parcel
+import android.os.Parcelable
+
 /**
+ * Represents an Image to display in Grid View
+ * THis class is parced to write in Intent or Bundle
  * Created on 12.10.17.
  */
 
-class Image {
+class Image() : Parcelable{
 
-    var uriImage : String?
-    var type : String?
-    var idIMage : Int?
+    var uriImage : String? = null
+    var type : String? = null
+    var idIMage : Int? = null
 
-    constructor(theUri : String, theType : String, theIdImage : Int) {
+    constructor(parcel: Parcel) : this() {
+        uriImage = parcel.readString()
+        type = parcel.readString()
+        idIMage = parcel.readValue(Int::class.java.classLoader) as? Int
+    }
+
+    constructor(theUri : String, theType : String, theIdImage : Int) : this() {
         this.uriImage = theUri
         this.type = theType
         this.idIMage = theIdImage
@@ -18,5 +29,25 @@ class Image {
 
     override fun toString() : String {
         return uriImage.plus(" - ").plus(type).plus(" - ").plus(idIMage)
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(uriImage)
+        parcel.writeString(type)
+        parcel.writeValue(idIMage)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Image> {
+        override fun createFromParcel(parcel: Parcel): Image {
+            return Image(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Image?> {
+            return arrayOfNulls(size)
+        }
     }
 }
