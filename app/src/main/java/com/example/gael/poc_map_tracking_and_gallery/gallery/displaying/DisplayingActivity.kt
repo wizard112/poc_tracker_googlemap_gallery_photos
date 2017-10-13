52dd80aea43cd14e4390bef214981649701f4471
Malcom <gaeltshilombo@gmail.com>
2017-10-13 13:10:17 +0200

    manage view pager

--- app/src/main/java/com/example/gael/poc_map_tracking_and_gallery/gallery/displaying/DisplayingActivity.kt
+++ app/src/main/java/com/example/gael/poc_map_tracking_and_gallery/gallery/displaying/DisplayingActivity.kt
@@ -4,24 +4,30 @@ import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.view.ViewPager
 import android.util.Log
 
 import com.example.gael.poc_map_tracking_and_gallery.R
 import com.example.gael.poc_map_tracking_and_gallery.Utils.Utils
+import com.example.gael.poc_map_tracking_and_gallery.adapters.ImageSlidePagerAdapter
 import com.example.gael.poc_map_tracking_and_gallery.models.Image
+import kotlinx.android.synthetic.main.activity_displaying.*
 
 class DisplayingActivity : AppCompatActivity() {
 
-    lateinit var presenter : DisplayingPresenter
+    //lateinit var presenter : DisplayingPresenter
 
     var images : ArrayList<Image>? = null
     var firstImage : Int? = null
     var defaultValueInt : Int = -1
 
+    var view_pager : ViewPager? = null
+    lateinit var myAdapter : ImageSlidePagerAdapter
+
 
     companion object {
         val KEY_LIST_IMAGES = "Key_images"
-        //selected image
+        //selected image to display first
         val KEY_SELECTED_IMAGE = "Key_selected_image"
 
         fun newIntent(cxt : Context, list : ArrayList<Image>, selectedImage : Int) : Intent {
@@ -39,14 +45,18 @@ class DisplayingActivity : AppCompatActivity() {
         getIntent(intent)
 
         //var frg : DisplayingFragment? = Utils.getFRagmentFromManager(this,R.id.container) as? DisplayingFragment
-        var frg : DisplayingFragment? = supportFragmentManager.findFragmentById(R.id.container) as? DisplayingFragment
+        /*var frg : DisplayingFragment? = supportFragmentManager.findFragmentById(R.id.container) as? DisplayingFragment
         if(frg == null){
             frg = DisplayingFragment.newInstance()
         }
 
         Utils.addFragmentFromManager(this,R.id.container,frg)
 
-        presenter = DisplayingPresenter(frg)
+        presenter = DisplayingPresenter(frg)*/
+
+        view_pager = viewPager
+        myAdapter = ImageSlidePagerAdapter(supportFragmentManager,this,images!!,firstImage!!)
+        view_pager!!.adapter = myAdapter
     }
 
     private fun getIntent(intent : Intent) {
