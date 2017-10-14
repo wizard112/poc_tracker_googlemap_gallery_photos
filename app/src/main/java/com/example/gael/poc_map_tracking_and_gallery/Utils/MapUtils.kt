8c67303d7e8175e4fa70a9e3ab214b85a3c61f0d
Malcom <gaeltshilombo@gmail.com>
2017-10-14 15:04:25 +0200

    add functiosn to manage polyline

--- app/src/main/java/com/example/gael/poc_map_tracking_and_gallery/Utils/MapUtils.kt
+++ app/src/main/java/com/example/gael/poc_map_tracking_and_gallery/Utils/MapUtils.kt
@@ -1,11 +1,22 @@
 package com.example.gael.poc_map_tracking_and_gallery.Utils
 
 import android.content.Context
+import android.location.Address
 import android.location.Geocoder
 import android.util.Log
+import com.example.gael.poc_map_tracking_and_gallery.Interfaces.IPoliline
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.LatLng
 import java.io.IOException
+import com.google.android.gms.vision.barcode.Barcode.GeoPoint
+import org.json.JSONException
+import com.google.maps.android.PolyUtil
+import org.json.JSONObject
+import org.json.JSONArray
+
+
+
+
 
 /**
  * Created on 12.10.17.
@@ -16,12 +27,18 @@ class MapUtils {
 
     companion object {
 
+        val KEY_ROUTES : String = "routes"
+        val KEY_OVERVIEW_POLYLINE : String = "overview_polyline"
+        val KEY_POINTS : String = "points"
+
         //types for map
         val MAP_TYPES = arrayOf(GoogleMap.MAP_TYPE_SATELLITE,
                 GoogleMap.MAP_TYPE_NORMAL,
                 GoogleMap.MAP_TYPE_HYBRID,
                 GoogleMap.MAP_TYPE_TERRAIN,
                 GoogleMap.MAP_TYPE_NONE)
+
+        var urlMapApiStatic : String = "http://maps.googleapis.com/maps/api/directions/json"
 
         /**
          * Allows to get address from position on map
@@ -38,6 +55,79 @@ class MapUtils {
                 }
             }
             return address!!
+        }
+
+        /**
+         * Allows to get position from address on map
+         */
+        fun getLatLngFromAddress(addres : String, ctxt : Context) : LatLng {
+            var location : Address? = null
+
+            if(addres != null && addres.isNotEmpty()) {
+                var geocoder : Geocoder  = Geocoder(ctxt)
+                var addresses : List<Address>
+
+                try {
+                    addresses = geocoder.getFromLocationName(addres,5)
+
+                    location = addresses.get(0)
+                    if(location.latitude.toString().length > 10) {
+                        val newLatitude : String = location.latitude.toString().subSequence(0,9).toString()
+                        location.latitude = newLatitude.toDouble()
+                    }
+                    //Log.i("Test",ttr)
+                    //Log.i("Test",location.latitude.toString().plus(",").plus(location.longitude))
+
+                }catch (ioe : IOException) {
+                    ioe.printStackTrace()
+                    Log.e("E",ioe.message)
+                }
+            }
+            return LatLng(location!!.latitude,location!!.longitude)
+        }
+
+        /**
+         * Get the poliline from origin and destination
+         */
+        fun getPointsInPoliline (origineLat : Double,originLng : Double,destLat : Double, destLng : Double, comm : IPoliline) {
+            var completeUrl : String = urlMapApiStatic.plus("?origin=")
+                    .plus(origineLat).plus(",").plus(originLng).plus("&destination=")
+                    .plus(destLat).plus(",")
+                    .plus(destLng).plus("&mode=driving")
+            Utils.executeUrl(completeUrl,comm)
+        }
+
+        /**
+         * INterpret the poliline
+         */
+        fun treatPoliline(result : String, comm : IPoliline){
+            var strPoints = StringBuilder()
+            var list : ArrayList<LatLng> = ArrayList()
+            try {
+                var mainObject : JSONObject = JSONObject(result)
+                if (mainObject.has(KEY_ROUTES)) {
+                    var routes = mainObject.getJSONArray(KEY_ROUTES)
+                    val firstIndex = routes.get(0) as JSONObject
+                    if (firstIndex.has(KEY_OVERVIEW_POLYLINE)) {
+                        val overviewPolyline = firstIndex.get(KEY_OVERVIEW_POLYLINE) as JSONObject
+                        val points = overviewPolyline.get(KEY_POINTS) as String
+                        val pr = PolyUtil.decode(points)
+                        for (p in pr) {
+                            list.add(p)
+                            val coord = p.latitude.toString() + "," + p.longitude
+                            strPoints.append(coord)
+                            strPoints.append("|")
+                        }
+                        strPoints.deleteCharAt((strPoints.length-1))
+                        //Log.i("Test",strPoints.toString())
+                        comm.sendPoints(list)
+                    }
+                }
+            } catch (e: JSONException) {
+                e.printStackTrace()
+                Log.e("E", "error json object " + e.message)
+            }
+
         }
     }
 }
--- app/src/main/java/com/example/gael/poc_map_tracking_and_gallery/Utils/Utils.kt
+++ app/src/main/java/com/example/gael/poc_map_tracking_and_gallery/Utils/Utils.kt
@@ -3,10 +3,17 @@ package com.example.gael.poc_map_tracking_and_gallery.Utils
 import android.app.Activity
 import android.content.Context
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.support.v4.app.ActivityCompat
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import com.example.gael.poc_map_tracking_and_gallery.Interfaces.IPoliline
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
 
 /**
  * Created on 12.10.17.
@@ -41,6 +48,31 @@ class Utils {
 
         fun addFragmentFromManager(cxt : AppCompatActivity, idContainer : Int, frg : Fragment) {
             cxt.supportFragmentManager.beginTransaction().add(idContainer,frg,"").commit()
+        }
+
+        /**
+         * Call Google map Api static to get poliline
+         */
+        fun executeUrl(theUrl : String, comm : IPoliline) : String {
+            var response : String? = null
+            Thread(Runnable {
+                // Do network action in this function
+                try {
+                    val url : URL = URL(theUrl)
+                    var httpUrlConnection : HttpURLConnection = url.openConnection() as HttpURLConnection
+                    if(httpUrlConnection.responseCode != HttpURLConnection.HTTP_OK){
+                        Log.i("Test","problem : ".plus(httpUrlConnection.responseCode))
+                    } else {
+                        response = httpUrlConnection.inputStream.bufferedReader().readText()
+                        //Log.i("Test",response)
+                        MapUtils.treatPoliline(response.toString(),comm)
+                    }
+                }catch (ioe : IOException){
+
+                }
+            }).start()
+
+            return response.toString()
         }
     }
 }
